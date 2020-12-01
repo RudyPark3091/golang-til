95fd2e2f764dd8258b724d189e1c15bdd41fe0d1
RudyPark3091 <grayblack313@gmail.com>
2020-12-01 20:56:52 +0900

    Update json/example2.js

--- json/example2.js
+++ json/example2.js
@@ -1,9 +1,21 @@
 const fs = require("fs")
+const assert = require("assert")
 
 var jsonData;
 
 fs.readFile("./example2.json", (err, data) => {
 	jsonData = JSON.parse(data)
 	console.log(jsonData)
+
+	assert.equal(jsonData.id, "0001")
+	assert.equal(jsonData.type, "donut")
+	assert.equal(jsonData.ppu, 0.55)
+	assert.equal(typeof jsonData.batters, "object")
+	assert.equal(typeof jsonData.batters.batter[0], "object")
+	assert.equal(jsonData.batters.batter[0].id, "1001")
+	assert.equal(jsonData.batters.batter[1].type, "Chocolate")
+	assert.equal(typeof jsonData.topping[0], "object")
+	assert.equal(jsonData.topping[0].id, "5001")
+	assert.equal(jsonData.topping[2].type, "Sugar")
 });
 
