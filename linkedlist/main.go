640ee104a92ccf38d068b7b18ffa2ae7615734ea
RudyPark3091 <grayblack313@gmail.com>
2020-12-18 00:59:17 +0900

    Add linkedlist

---
+++ linkedlist/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	value int
+	link  *Node
+}
+
+func (n *Node) append(node *Node) *Node {
+	n.link = node
+	return n
+}
+
+func main() {
+	n1 := &Node{1, nil}
+	n2 := &Node{2, nil}
+	n3 := &Node{3, nil}
+	n4 := &Node{4, nil}
+
+	n3.append(n4)
+	n2.append(n3)
+	n1.append(n2)
+
+	fmt.Println(n1.value)                // 1
+	fmt.Println(n1.link.value)           // 2
+	fmt.Println(n1.link.link.value)      // 3
+	fmt.Println(n1.link.link.link.value) // 4
+}
