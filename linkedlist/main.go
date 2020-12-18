ac09227bdae7f52e07e6dc4f82b35210f3cc7704
RudyPark3091 <grayblack313@gmail.com>
2020-12-18 13:08:35 +0900

    Fix typos

--- linkedlist/main.go
+++ linkedlist/main.go
@@ -7,7 +7,7 @@ type Node struct {
 	link  *Node
 }
 
-func (n *Node) Append(node *Node) *Node {
+func (n *Node) Append(node *Node) {
 	n.link = node
 }
 
@@ -17,12 +17,23 @@ func main() {
 	n3 := &Node{3, nil}
 	n4 := &Node{4, nil}
 
-	n3.append(n4)
-	n2.append(n3)
-	n1.append(n2)
+	n3.Append(n4)
+	n2.Append(n3)
+	n1.Append(n2)
 
 	fmt.Println(n1.value)                // 1
 	fmt.Println(n1.link.value)           // 2
 	fmt.Println(n1.link.link.value)      // 3
 	fmt.Println(n1.link.link.link.value) // 4
+
+	fmt.Println()
+
+	node := n1
+	fmt.Println(node.value) // 1
+	node = node.link
+	fmt.Println(node.value) // 2
+	node = node.link
+	fmt.Println(node.value) // 3
+	node = node.link
+	fmt.Println(node.value) // 4
 }
